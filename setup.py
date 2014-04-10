734484caa5846d478fe3a9f542575a7c87f71084
Christian Alis <c.alis@ucl.ac.uk>
2014-04-10 07:01:17 +0100

    Added support for older setuptools

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
 
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name = "multishelve",
     version = "0.1dev",
-    packages = find_packages("multishelve"),
+    packages = ["multishelve"],
     test_suite = "test_multishelve.MultishelveTest",
     tests_require = "mox"
 )
